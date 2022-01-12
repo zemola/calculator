ad57712dcd7a4310480f5e74b96c709996751272
unknown <azeemolatunji@gmail.com>
2022-01-12 08:49:56 +0100

    Complete styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
 import Mainbody from './componets/Calculator';
+import Home from './componets/Home';
+import Quote from './componets/quote';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class App extends React.Component {
-  render() {
-    return (
-      <div className="general-wrapper">
-        <h1>Calculator</h1>
-        <Mainbody />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <BrowserRouter>
+    <div className="wrapper-er">
+      <nav className="nav">
+        <h1 className="heading">Math Magicians</h1>
+        <ul>
+          <Link to="/">Home</Link>
+          <Link to="/calculator">Calculator</Link>
+          <Link to="/quotes">Quote</Link>
+
+        </ul>
+      </nav>
+    </div>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/calculator" element={<Mainbody />} />
+      <Route path="/quotes" element={<Quote />} />
+
+    </Routes>
+  </BrowserRouter>
+);
+
 export default App;
